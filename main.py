d9e07dfce511c89d8601cf649e6641fdbf5d054d
leparwa <kevin.leparwa@student.moringaschool.com>
2021-08-05 18:03:10 +0300

    comments added main.py file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from app import  loginUser, registerUser
+
+#functions in the app folder declaration
 register_func = registerUser
 login_func=loginUser.login
+
+
 def userPrompt():
     userOptions=""
     while userOptions != '1' or userOptions != '2':
@@ -9,11 +13,14 @@ def userPrompt():
                             "\n1: Login\n-------------------"
                             "\n2: Create Account\n-------------------"
                             "\n3:  Exit \n-------------------\n>>> ")
+        #logic if user chose login
         if userOptions =='1':
             print("\033c")
             username=input("Enter Your Account Username \n >> ")
             passWord=input("Enter Your Account Password \n >> ")
-            login_func(username)
+            login_func(username, passWord)
+
+        #logic if user chose Create Account
         elif userOptions=='2':
             print("\033c")
             print("Welcome, you are one step from managing your password \n"
@@ -21,16 +28,9 @@ def userPrompt():
             userName=input("Enter Your Username \n >> ")
             passWord=input("Enter Password \n >> ")
             repeatPassWord=input("Repeat Password \n >> ")
-
+    
+        #logic if user opted to exit
         else:
             exit("Exit Successful")
 
-def createAction():
-    print("successfull creating Account")
-
-def loginUser():
-    print("login success")
-
-def manageUserActivities():
-    print("")
-userPrompt()    
+userPrompt()
