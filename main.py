e8a865fe63d7c45d16c3e301c5cdfeaad1ee4e24
leparwa <kevin.leparwa@student.moringaschool.com>
2021-08-05 16:33:38 +0300

    manage user activity functionality added

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-userOptions=""
-while userOptions != '1' or userOptions != '2':
-    userOptions= input("WELCOME TO YOUR PASSWORD MANAGER PORTAL !! \n"
-                        "*****************************************"
-                        "\n1: Login\n-------------------"
-                        "\n2: Create Account\n-------------------"
-                        "\n3:  Exit \n-------------------\n>>> ")
-    if userOptions =='1':
-        print("\033c")
-        print("Welcome To Your Account")
-        userName=input("Enter Your Account Username")
-        passWord=input("Enter Your Account Password")
+def userPrompt():
+    userOptions=""
+    while userOptions != '1' or userOptions != '2':
+        userOptions= input("WELCOME TO YOUR PASSWORD MANAGER PORTAL !! \n"
+                            "*****************************************"
+                            "\n1: Login\n-------------------"
+                            "\n2: Create Account\n-------------------"
+                            "\n3:  Exit \n-------------------\n>>> ")
+        if userOptions =='1':
+            print("\033c")
+            userName=input("Enter Your Account Username \n >> ")
+            passWord=input("Enter Your Account Password \n >> ")
 
-    elif userOptions=='2':
-        print("\033c")
-        print("Welcome, you are one step from managing your password \n"
-            "******************************************************")
-        userName=input("Enter Your Username")
-        passWord=input("Enter Password")
-        repeatPassWord=input("Repeat Password")
-    else:
-        exit("Exit Success")
+        elif userOptions=='2':
+            print("\033c")
+            print("Welcome, you are one step from managing your password \n"
+                "******************************************************")
+            userName=input("Enter Your Username \n >> ")
+            passWord=input("Enter Password \n >> ")
+            repeatPassWord=input("Repeat Password \n >> ")
+            createAction()
 
+        else:
+            exit("Exit Successful")
 
+def createAction():
+    print("successfull creating Account")
+
+def loginUser():
+    print("login success")
+
+def manageUserActivities():
+    print("")
+userPrompt()    
