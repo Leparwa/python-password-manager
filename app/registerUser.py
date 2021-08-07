fc7b9d2c923b0e2eb541aab72f2a27aea17a7f58
leparwa <kevin.leparwa@student.moringaschool.com>
2021-08-08 00:47:20 +0300

    solved circular imports at main.py file

--- app/registerUser.py
+++ app/registerUser.py
@@ -1,7 +1,5 @@
 from app.manageFile import writeRegisterUserFile
-from main import userPrompt
 
-repeatProces =userPrompt
 
 write_register_func = writeRegisterUserFile
 def registerUser(userName, password, repeatPassword):
@@ -11,7 +9,6 @@ def registerUser(userName, password, repeatPassword):
         actionsAfterRegister(userName)        
     elif(password != repeatPassword):
         print("passwords do not match")
-        repeatProces()
     else:
         exit()
 def actionsAfterRegister(user):
